bf5a17aeb9220319d307a7199b7849e1f5b1bc69
abhijeetsawant-lab <abhijeetsawant3033@gmail.com>
2026-07-09 06:03:56 +0530

    first program for this

---
+++ INPUT_OUTPUT/hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    cout << "hello world" << endl << 877;
+    cout << endl;
+    int a;
+    cin >> a;
+    cout << "you have entererd a value : " << a;
+return 0;
+}
